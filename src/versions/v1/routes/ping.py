4a850c6c7b33b9eb09288a3c8059aab6ad1292ac
Anivesh nishad <78677890+Aniveshnishad76@users.noreply.github.com>
2024-01-08 16:45:59 +0530

    Update ping.py

--- src/versions/v1/routes/ping.py
+++ src/versions/v1/routes/ping.py
@@ -2,13 +2,10 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
-from src.tasks.split_calculation import split_amount_task
-
 router = APIRouter()
 
 
 @router.get("")
 async def ping():
     """Ping"""
-    await split_amount_task(_id=16)
     return JSONResponse(content={"service": "ok"}, status_code=200)
